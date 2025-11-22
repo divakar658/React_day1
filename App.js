26f16129bac699017a701857fb3e5443dde196e0
allaboina divakar <132629933+divakar658@users.noreply.github.com>
2025-11-22 12:15:06 +0530

    header in react

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-// const heading=React.createElement("h1",{id:"heading"},"Hello World From React!!!");
-//             const root=ReactDOM.createRoot(document.getElementById("root"));
-//             root.render(heading);
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"HEllO From inside of the container"),
-            React.createElement("h1",{},"HEllO From inside of the container")]
-    )
+import React from "react";
+import ReactDOM from "react-dom/client"; // 👈 important difference
+import Header  from "./Header";
+import "./Header.css";
+const Heading=()=>(
+    <div>
+        <h1 id="heading">Hello Divakar from react</h1>,
+        {title}
+        <h1>Hello raina</h1>
+    </div>
+);
+const title=(<div>
+    <h1>React Project</h1>
+    <h1>Java Project</h1>
+</div>
 );
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Header/>);
